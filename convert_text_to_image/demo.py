8745157da6bf3124a773ee78debe29257da935b5
Stream <mystream@vip.qq.com>
2016-06-23 16:43:09 +0800

    support chinese

--- convert_text_to_image/demo.py
+++ convert_text_to_image/demo.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 # only support python2
 # TODO support Chinese...
+#import uniout # support list output chinese, sudo pip install uniout
 import Image, ImageFont, ImageDraw
-import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-FONT = "/usr/share/fonts/truetype/freefont/FreeSans.ttf"
+FONT = "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf"
 
-print "make sure the file your want to convert in the same path."
-filename = raw_input("input the filename you want to convert: ")
-
+# print "make sure the file your want to convert in the same path."
+# filename = raw_input("input the filename you want to convert: ")
+filename = 'chinese.txt'
 with open(filename) as f:
     lines = f.readlines()
 
@@ -21,7 +24,8 @@ print inputfile
 
 font = ImageFont.truetype(FONT, 16)
 
-output = filename.split('.')[0] + '.png'
+# output = filename.split('.')[0] + '.png'
+output = 'chinese.png'
 
 def text2png(inputfile, output, fontpath=None,
              fontsize=16, color='#000', bgcolor='#FFF',
@@ -45,7 +49,10 @@ def text2png(inputfile, output, fontpath=None,
     text = inputfile
 
     lines = process_text(text, fixed_size)
-    print lines
+    for line in lines:
+        # print  str(line).decode('string_escape')
+        print line
+
     line_height = font.getsize(text)[1]
     img_height = line_height * (len(lines) + 3)
 
@@ -69,41 +76,73 @@ def process_text(text, fixed_size):
     line = u""
     word = u""
     for char in text:
-        if font.getsize(line+word)[0] <= fixed_size:
-            if char != '\n':
-                if char == ' ':
-                    in_word = False
-                    line += word
-                    word = u""
-                    # join space
-                    line += char
-                else:
-                    in_word = True
-                    word += char
-            # meet LF
-            else:
+        print char
+        print 'line is:', line
+        print 'lines is:', lines
+        num = ord(char.decode('utf-8'))
+        print num
+        if num < 128:
+            lines, line, word, in_word = parse_ascii(char,
+                                                     fixed_size,
+                                                     lines,
+                                                     line,
+                                                     word,
+                                                     in_word)
+        else:
+            lines, line = parse_chinese(char, fixed_size, lines, line)
+
+    return lines
+
+def parse_chinese(char, fixed_size, lines, line):
+    if font.getsize(line)[0] <= fixed_size:
+        if char != '\n':
+            line += char
+        else:
+            lines.append(line)
+            line = u""
+    else:
+        lines.append(line)
+        line = char
+    return lines, line
+
+
+def parse_ascii(char, fixed_size, lines, line, word, in_word):
+    if font.getsize(line+word)[0] <= fixed_size:
+        if char != '\n':
+            if char == ' ':
                 in_word = False
                 line += word
                 word = u""
-                lines.append(line)
-                line = u""
-        # > fixed_size should append
-        else:
-            # last char was space or LF
-            if in_word == False:
-                # word can be add to line
-                line += word
-                # replace word with new char
-                word = char
-                lines.append(line)
-                line = u""
-            # last char was not space or LF
+                # join space
+                line += char
             else:
-                # add char to current word and keep it
+                in_word = True
                 word += char
-                lines.append(line)
-                line = u""
-    return lines
+        # meet LF
+        else:
+            in_word = False
+            line += word
+            word = u""
+            lines.append(line)
+            line = u""
+    # > fixed_size should append
+    else:
+        # last char was space or LF
+        if in_word == False:
+            # word can be add to line
+            line += word
+            # replace word with new char
+            word = char
+            lines.append(line)
+            line = u""
+        # last char was not space or LF
+        else:
+            # add char to current word and keep it
+            word += char
+            lines.append(line)
+            line = u""
+    return lines, line, word, in_word
+
 
 if __name__ == "__main__":
     text2png(inputfile, output, FONT)
